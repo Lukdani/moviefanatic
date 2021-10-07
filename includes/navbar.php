5614735653c362f4e87a419524102fe9290fadbf
Urte-au <urte0006@edu.easj.dk>
2021-10-07 10:37:30 +0200

    actors

--- includes/navbar.php
+++ includes/navbar.php
@@ -13,7 +13,7 @@
                 <a class="nav-link" href="/moviefanatic/index.php">Movies</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Actors</a>
+                <a class="nav-link" href="/moviefanatic/actors.php">Actors</a>
             </li>
         </ul>
 
