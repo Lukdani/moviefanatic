b1c1733c003ab158a756e415d071a036fd399a9b
lukaspedersen <ldp@sweetgeeks.dk>
2021-10-12 11:10:24 +0200

    try fix delete movie

--- api/deleteMovie.php
+++ api/deleteMovie.php
@@ -1,7 +1,7 @@
 <?php 
 require_once $_SERVER['DOCUMENT_ROOT'] . "/moviefanatic/settings/init.php";
 
-$movieId = number_format($_REQUEST["movieId"]);
+$movieId = $_REQUEST["movieId"];
 
 $deleteQuery = "DELETE FROM movies WHERE movieId=:movieId";
 $bind = [":movieId" => $movieId];
